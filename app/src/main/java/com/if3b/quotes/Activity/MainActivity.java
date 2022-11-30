2b9ffd507df4053c88c35c5cee1cb66032aeac2c
DarkLeZx <depniel08@mhs.mdp.ac.id>
2022-11-30 14:15:51 +0700

    Pembuatan Package

--- app/src/main/java/com/if3b/quotes/Activity/MainActivity.java
+++ app/src/main/java/com/if3b/quotes/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.if3b.quotes;
+package com.if3b.quotes.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.if3b.quotes.R;
 
 public class MainActivity extends AppCompatActivity {
 
