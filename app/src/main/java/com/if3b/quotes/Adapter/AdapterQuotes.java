90c66f5724fe3ffa57604faa77b4404c2751d33e
DarkLeZx <depniel08@mhs.mdp.ac.id>
2022-12-05 10:15:58 +0700

    AdapterQuotes.VH
    Extends Class

--- app/src/main/java/com/if3b/quotes/Adapter/AdapterQuotes.java
+++ app/src/main/java/com/if3b/quotes/Adapter/AdapterQuotes.java
@@ -2,6 +2,7 @@ package com.if3b.quotes.Adapter;
 
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,8 +10,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.if3b.quotes.R;
 
-public class AdapterQuotes
+public class AdapterQuotes extends RecyclerView.Adapter<AdapterQuotes.ViewHolderQuotes>
 {
+    @NonNull
+    @Override
+    public ViewHolderQuotes onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderQuotes holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ViewHolderQuotes extends RecyclerView.ViewHolder
     {
         TextView tvQuotes;
